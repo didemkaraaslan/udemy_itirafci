6695fd9075ba9aaefac22a9c66b203976d430b6a
Ashley20 <kucukkaraaslan.didem@gmail.com>
2020-11-13 12:46:00 +0300

    Login UI

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Grid,
+  Segment,
+  Form,
+  Header,
+  Button,
+  Icon,
+  Message,
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import styles from './auth.module.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ email, password });
+  };
+
+  const displayErrors = () =>
+    errors.map((error, key) => <p key={key}> {error.message}</p>);
+
+  return (
+    <Grid textAlign="center" verticalAlign="middle" className={styles.form}>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="violet" icon>
+          <Icon name="comment alternate outline" />
+          {" İtirafci'ya Giriş Yap "}
+        </Header>
+
+        <Form>
+          <Segment>
+            <Form.Field>
+              <Form.Input
+                fluid
+                placeholder="Email Adresi"
+                onChange={(e) => setEmail(e.target.value)}
+                icon="mail"
+                iconPosition="left"
+                name="email"
+                type="email"
+              />
+            </Form.Field>
+            <Form.Field>
+              <Form.Input
+                fluid
+                placeholder="Parola"
+                onChange={(e) => setPassword(e.target.value)}
+                icon="lock"
+                iconPosition="left"
+                name="password"
+                type="password"
+              />
+            </Form.Field>
+          </Segment>
+
+          <Button color="green" fluid size="large" onClick={handleSubmit}>
+            Griş Yap
+          </Button>
+        </Form>
+        {errors.length > 0 && <Message error>{displayErrors()}</Message>}
+        <Message>
+          <Icon name="help" />
+          Hesabınız yok mu? <Link to="/register">Hesap Oluştur</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default Login;
