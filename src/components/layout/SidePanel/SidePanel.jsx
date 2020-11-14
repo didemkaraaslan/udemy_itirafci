1c8dac4f11cb1a2872d266f9395c6e07c16f5bfe
didem <didem.kucukkaraaslan@altosec.com.tr>
2020-11-14 12:07:56 +0300

    sidepanel

--- src/components/layout/SidePanel/SidePanel.jsx
+++ src/components/layout/SidePanel/SidePanel.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Menu } from "semantic-ui-react";
+import * as Tag from "../../../utils/Tags";
 
 const SidePanel = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  return (
+    <Menu
+      vertical
+      fixed="left"
+      pointing
+      style={{
+        marginTop: 61,
+        width: 380,
+        fontSize: "1.1rem",
+      }}
+    >
+      {/* Genel Kategoriler */}
+      <Menu.Item>
+        <Menu.Header>Kategoriler</Menu.Header>
+        <Menu.Menu>
+          <Menu.Item name={Tag.ALL} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.REGRET} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.FIRST_EXPERIENCE} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.SAD} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.GUILTY} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.LOVE} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.HAPPY} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.CHEATING} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.MOCKING} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.SEXUAL_ABUSE} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.CONGRATULATIONS} as="a" icon="hashtag" />
+        </Menu.Menu>
+      </Menu.Item>
+      {/* Özel Kategoriler */}
+      <Menu.Item>
+        <Menu.Header>Özel Kategoriler</Menu.Header>
+        <Menu.Menu>
+          <Menu.Item name={Tag.NOT_ANONYMOUS} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.ONLY_ANONYMOUS} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.MOST_APPROVED} as="a" icon="hashtag" />
+          <Menu.Item name={Tag.MOST_JUDGED} as="a" icon="hashtag" />
+        </Menu.Menu>
+      </Menu.Item>
+    </Menu>
+  );
 };
 
 export default SidePanel;
---
+++ src/utils/Tags.js
@@ -0,0 +1,36 @@
+/* Özel Kategoriler */
+const NOT_ANONYMOUS = "Not Anonymous";
+const ONLY_ANONYMOUS = "Only Anonymous";
+const MOST_APPROVED = "Most Approved";
+const MOST_JUDGED = "Most Judged";
+
+/* Genel Kategoriler */
+const ALL = "All";
+const REGRET = "Regret";
+const FIRST_EXPERIENCE = "First Experience";
+const SAD = "Sad";
+const GUILTY = "Guilty";
+const LOVE = "Love";
+const HAPPY = "Happy";
+const CONGRATULATIONS = "Congratulations";
+const CHEATING = "Cheating";
+const MOCKING = "Mocking";
+const SEXUAL_ABUSE = "Sexual Abuse";
+
+export {
+  NOT_ANONYMOUS,
+  ONLY_ANONYMOUS,
+  MOST_APPROVED,
+  MOST_JUDGED,
+  ALL,
+  REGRET,
+  FIRST_EXPERIENCE,
+  SAD,
+  GUILTY,
+  LOVE,
+  HAPPY,
+  CONGRATULATIONS,
+  CHEATING,
+  MOCKING,
+  SEXUAL_ABUSE,
+};
